ab3b3463bfeeccaa66a69e4883dcb48d73f0e535
retheviper <retheviper@gmail.com>
2023-07-02 21:15:37 +0900

    fix

--- api/src/main/kotlin/com/retheviper/choseikun/Application.kt
+++ api/src/main/kotlin/com/retheviper/choseikun/Application.kt
@@ -11,8 +11,12 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+    embeddedServer(
+        factory = Netty,
+        port = 8080,
+        host = "0.0.0.0",
+        module = Application::module
+    ).start(wait = true)
 }
 
 fun Application.module() {
